ec310c05b2020ae28031c8b76139131e6febd6fe
terx7 <erkileemet10@gmail.com>
2020-10-15 14:19:39 +0300

    yes

--- tul/tul.py
+++ tul/tul.py
@@ -3,14 +3,11 @@ f = open("tul/tulsis0.txt", "r")
 
 n = int(f.readline())
 print(n)
-people = {
-    'firstName' : {
-
-    }
-}
+people = {}
 
 for i in range(n):
     per = f.readline()
     perList = per.split()
-    firstName = perList[0]
-    if firstName not in people:
+    nameList = perList[0]
+    if nameList[0] not in people:
+        people[nameList[0]] = {}
